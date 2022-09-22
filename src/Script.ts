582a630439794f9364508bc17e2b8e0282e77358
Arthur <arthuremeunome@gmail.com>
2022-09-22 17:19:01 -0300

    Exercicio

--- src/Script.ts
+++ src/Script.ts
@@ -1,10 +1,14 @@
-import PromptSync = require('prompt-sync');
-
+import PromptSync = require('prompt-sync')
 const prompt = PromptSync()
 
-const calc = (peso: number, altura:number) => {
-    const imc = peso / (Math.pow(altura,2))
-    return imc
+const imc = (peso: number, altura: number) => { return peso / (Math.pow(altura, 2));
 }
 
-console.log(calc(80, 2.0));
+console.log('Digite seu peso: ')
+const peso = Number(prompt(`--> `));
+
+console.log(`Digite sua altura: `)
+const altura = Number(prompt(`--> `));
+
+const imResult = imc(peso, altura)
+console.log(`Seu IMC é ${imResult}`)
